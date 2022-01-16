9d2146d514144c1cce439a92fb1e63fa37f6a89f
Marek Czyż <Marek.Czyz@dreamlab.pl>
2022-01-16 20:51:24 +0100

    Searching events based on it's geolocation if location is awailable

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,25 @@ import Layout from '../components/layout'
 import styles from '../styles/Home.module.scss'
 import useSWR from 'swr'
 import { fetcher } from '../helpers'
+import geohash from 'ngeohash'
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
-  const { data, error } = useSWR('https://app.ticketmaster.com/discovery/v2/events.json?apikey=HN1QS3e5ZB3VZcJEK3xGpoK5HQmtdWUK&geoPoint=u3k41hk9&radius=50&unit=km&size=5', fetcher);
+  const [positioned, setPositioned] = useState(false)
+  const [geoPoint, setGeoPoint] = useState('')
+  const { data, error } = useSWR(positioned ? `https://app.ticketmaster.com/discovery/v2/events.json?apikey=HN1QS3e5ZB3VZcJEK3xGpoK5HQmtdWUK&geoPoint=${geoPoint}&radius=50&unit=km&size=10` : null, fetcher);
+  useEffect(() => {
+    if('geolocation' in navigator) {
+      /* geolocation is available */
+      navigator.geolocation.getCurrentPosition((position) => {
+        setGeoPoint(geohash.encode(position.coords.latitude, position.coords.longitude));
+      });    
+      setPositioned(true)
+    } else {
+      /* geolocation IS NOT available */
+      setPositioned(true)
+    }  
+  }, []);  
   return (
     <Layout>
       <div className={styles.container}>
@@ -20,14 +36,14 @@ export default function Home() {
               !data ? <div>loading...</div> :
                 <div className={styles.grid}>
                   {
-                    data._embedded.events.map(evt => {
+                    data?._embedded?.events.map(evt => {
                       return (
-                        <a href={evt.url} className={styles.card}>
+                        <a key={evt.id} href={evt.url} className={styles.card}>
                           <h2>{evt.name} &rarr;</h2>
                           <p>To find more info click a link.</p>
                         </a>
                       )
-                    })
+                    }) ?? <div>There is no events</div>
                   }
                 </div>
           }
